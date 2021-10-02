c74b4960c21bd15c396e9e922baebfb809f679da
Ollenurb <brunello.mtt@gmail.com>
2021-10-02 17:20:43 +0200

    [Misc] Deleted WEEKTODO.md (moved to ncortex), modified shell.nix under AAUT

--- AAut-INF0091/Lab/shell.nix
+++ AAut-INF0091/Lab/shell.nix
@@ -1,21 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  mach-nix = import (
-    builtins.fetchGit {
-      url = "https://github.com/DavHau/mach-nix/";
-      ref = "2.0.0";
-    }
-  );
+  # import mach-nix
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix";
+    ref = "refs/tags/3.3.0";
+  }) {};
 
   overridedPython = mach-nix.mkPython {
-    python = pkgs.python38;
+    python = "python38";
     requirements = ''
       scikit-learn
       matplotlib
       pydotplus
       graphviz
-      jupyter
+      jupyterlab
     '';
   };
 in
